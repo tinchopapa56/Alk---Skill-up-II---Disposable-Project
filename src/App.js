39c5cb35326dc3f45ad22702a85314c401fddd9c
CodingWithTincho <martinlabastie.p@gmail.com>
2022-08-01 15:44:20 -0300

    same as before bute WORKING

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import {Route, Routes, Navigate} from "react-router-dom"
+import { AnimatePresence, motion } from 'framer-motion';
+
 import FormikLogin from "./components/unidad 3 - formik/formik Login"
 import Error404 from "./components/views/Error404/Error404"
 import {Tasks} from "./components/views/Tasks/Tasks"
 
-const RequireAuth = (children)=>{
+const RequireAuth = ( {children} )=>{
     if(!localStorage.getItem("logged")){
         return <Navigate to="/login" replace={true} />
     } else{
@@ -13,14 +15,17 @@ const RequireAuth = (children)=>{
 }
 
 export const App =() => 
-    <Routes> 
-        <Route 
-            path="/Tasks" 
-            element={
-                <RequireAuth> 
-                  <Tasks />
-                </RequireAuth> 
-                } />
-        <Route path="/login" element={<FormikLogin />} />
-        <Route path="*" element={<Error404 />} />
-    </Routes>
+    <AnimatePresence>
+        <Routes> 
+            <Route 
+                path="/" 
+                element={
+                    <RequireAuth> 
+                    <Tasks />
+                    </RequireAuth> 
+                    }
+                />
+            <Route path="/login" element={<FormikLogin />} />
+            <Route path="*" element={<Error404 />} />
+        </Routes>
+    </AnimatePresence>
